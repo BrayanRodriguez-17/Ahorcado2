358a4ab9011a59752cc325a965d721df3f30fc21
BrayanRodriguez-17 <158258721+BrayanRodriguez-17@users.noreply.github.com>
2024-01-31 11:43:11 -0600

    Add files via upload

---
+++ ahoracado modificado.py
@@ -0,0 +1,61 @@
+class Ahorcado:
+    def __init__(self):
+        self.palabra_secreta = "Ahorcado"
+        self.letras_correctas = []
+        self.intentos_restantes = 8
+        self.muñeco = ["  O", " \|/", "  |", " / \\"]
+
+    def mostrar_palabra(self):
+        resultado = ''
+        for letra in self.palabra_secreta:
+            resultado += letra if letra in self.letras_correctas else '_'
+            resultado += ' '
+        return resultado
+
+    def mostrar_muñeco(self):
+        resultado = ''
+        for i in range(8 - self.intentos_restantes):
+            resultado += self.muñeco[i]
+        return resultado
+
+    def jugar(self):
+        print("¡Bienvenido al Juego del Ahorcado!")
+        print("Descubre la palabra oculta. ¡Buena suerte!")
+
+        while self.intentos_restantes > 0:
+            print(f"{self.mostrar_muñeco()}")
+            print(self.mostrar_palabra())
+
+            letra_usuario = self.obtener_letra()
+            if letra_usuario in self.palabra_secreta and letra_usuario not in self.letras_correctas:
+                self.letras_correctas = [letra_usuario] + self.letras_correctas
+                print(f"¡Correcto! Letras correctas: {', '.join(self.letras_correctas)}")
+            else:
+                self.intentos_restantes -= 1
+                print(f"Incorrecto. Te quedan {self.intentos_restantes} intentos.")
+
+            if set(self.palabra_secreta).issubset(self.letras_correctas):
+                print("¡Felicidades! ¡Has adivinado la palabra!")
+                break
+
+        self.opcion_reiniciar()
+
+    def obtener_letra(self):
+        while True:
+            letra = input("Ingresa una letra: ")
+            if letra:
+                return letra
+            else:
+                print("Por favor, ingresa un carácter.")
+
+    def opcion_reiniciar(self):
+        reiniciar = input("¿Quieres jugar de nuevo? (Sí/No): ").lower()
+        if reiniciar == 'si':
+            juego = Ahorcado()
+            juego.jugar()
+        else:
+            print("Gracias por jugar. ¡Hasta luego!")
+
+# Inicializar el juego
+juego = Ahorcado()
+juego.jugar()
